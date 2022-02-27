998f88ba547e1e1b8dc3c88bce98deb2c404f46b
akuhaell23 <farahnurin12@gmail.com>
2022-02-27 22:26:15 +0700

    ukk

---
+++ frontend/admin-laundry/src/pages/paket.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Navbar from "../component/navbar"
+export default class Home extends React.Component{
+    constructor(){
+        super()
+    }
+    render(){
+        return(
+            <div> 
+                <Navbar />
+                <h1>Ini Halaman Paket</h1>
+            </div>
+        )
+    }
+}
