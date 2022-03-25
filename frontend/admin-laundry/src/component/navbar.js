6d8ea5f0487055209d9cc9ef8f19f0df1fd73818
Rahil <53207506+akuhaell23@users.noreply.github.com>
2022-03-25 17:04:30 +0700

    Update navbar.js

--- frontend/admin-laundry/src/component/navbar.js
+++ frontend/admin-laundry/src/component/navbar.js
@@ -28,22 +28,91 @@ class Navbar extends React.Component{
                             </Link>
                         </li>
                         <li className="nav-item">
-                            <Link to="/product" className="nav-link">
+                            <Link to="/member" className="nav-link">
                                 Member
                             </Link>
                         </li>
-                        <li className="nav-item">
-                            <Link to="/customer" className="nav-link">
+                      {this.props.role !== "kasir"?(  <li className="nav-item">
+                            <Link to="/user" className="nav-link">
                                 User
                                 </Link>
                         </li>
+                        ):null}
+                        {this.props.role !== "kasir"?( 
+                             <li className="nav-item">
+                             <Link to="/paket" className="nav-link">
+                                 Paket
+                             </Link>
+                         </li>
+                        ):null}
+                       
                         <li className="nav-item">
-                            <Link to="/transaction" className="nav-link">
-                                Paket
+                            <Link to="/transaksi" className="nav-link">
+                                Transaksi
                             </Link>
                         </li>
                         <li className="nav-item">
-                            <Link to="/admin" className="nav-link">
+                            <Link className="nav-link" onClick={() => this.Logout()}>
+                                Logout
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+}
+export default Navbar;
+import React from "react"
+import {Link} from "react-router-dom"
+class Navbar extends React.Component{
+    Logout = () => {
+        localStorage.removeItem("token")
+        localStorage.removeItem("admin")
+        window.location = "/login"
+    }
+    render(){
+        return(
+            <div className="navbar navbar-expand-lg bg-dark navbar-dark">
+                <a className="navbar-brand">
+                    Moklet Laundry
+                </a>
+
+                {/* show and hide menu */}
+                <button className="navbar-toggler" data-toggle="collapse"
+                data-target="#menu">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                {/* menu */}
+                <div id="menu" className="navbar-collapse collpase">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item">
+                            <Link to="/" className="nav-link">
+                                Home
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/member" className="nav-link">
+                                Member
+                            </Link>
+                        </li>
+                      {this.props.role !== "kasir"?(  <li className="nav-item">
+                            <Link to="/user" className="nav-link">
+                                User
+                                </Link>
+                        </li>
+                        ):null}
+                        {this.props.role !== "kasir"?( 
+                             <li className="nav-item">
+                             <Link to="/paket" className="nav-link">
+                                 Paket
+                             </Link>
+                         </li>
+                        ):null}
+                       
+                        <li className="nav-item">
+                            <Link to="/transaksi" className="nav-link">
                                 Transaksi
                             </Link>
                         </li>
