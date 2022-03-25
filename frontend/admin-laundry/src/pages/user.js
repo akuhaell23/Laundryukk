289f1224f0ad35b85866081c39cbc7e1fdd06ee2
Rahil <53207506+akuhaell23@users.noreply.github.com>
2022-03-25 17:08:36 +0700

    Update user.js

--- frontend/admin-laundry/src/pages/user.js
+++ frontend/admin-laundry/src/pages/user.js
@@ -2,17 +2,17 @@ import React from "react"
 import Navbar from "../component/navbar"
 // import base_url dari file config.js
 import { base_url } from "../config"
-
+ 
 // import axios
 import axios from "axios"
-
+ 
 // import jquery
 import $ from "jquery"
-
+ 
 // import modal -> untuk versi bootstrap 5
 // import { Modal } from "bootstrap"
 // note : jika menggunakan bootstrap 4 tidak ush menambahkan script diatas
-
+ 
 export default class User extends React.Component{
     constructor(){
         super()
@@ -27,82 +27,128 @@ export default class User extends React.Component{
             role: "",
             fillPassword: true            
         }
-
+ 
         /* logika if-else --> untuk mengecek apakah user yg mengakses telah melakukan
            login sebagai admin atau belum
         */
         if (localStorage.getItem("token")) {
             this.state.token = localStorage.getItem("item")
+            this.state.role = JSON.parse(localStorage.getItem("user")).role
         } else {
             window.location = "/login"
         }
     }
-
-// header config -> untuk memberikan header berupa 'beare token' sebagai request API
-// sebelum mengakses data
-    headerConfig = () => {
-        let header = {
-            headers: { Authorization: `Bearer ${this.state.token}`}
-        }
-        return header
-    }
-
-// getUser -> untuk mengakses API get user
-    getUser = () => {
-        let url = base_url + "/user"
-        axios.get(url, this.headerConfig())
-        .then(response => {
-            this.setState({user: response.data})
-        })
-        .catch(error => {
-            if (error.response) {
-                if (error.response.status) {
-                    window.alert(error.response.data.message)
-                    this.props.history.push("/login")
-                }
-            } else {
-                console.log(error);
+    // header config -> untuk memberikan header berupa 'beare token' sebagai request API
+    // sebelum mengakses data
+        headerConfig = () => {
+            let header = {
+                headers: { Authorization: `Bearer ${this.state.token}`}
             }
-        })
-    }
+            if( this.state.role !== "admin") this.props.history.push("/notfound")
 
-    componentDidMount() {
+            return header
+        }
+    // getUser -> untuk mengakses API get user
+        getUser = () => {
+            let url = base_url + "/user"
+            axios.get(url, this.headerConfig())
+            .then(response => {
+                this.setState({user: response.data})
+            })
+            .catch(error => {
+                if (error.response) {
+                    if (error.response.status) {
+                        window.alert(error.response.data.message)
+                        this.props.history.push("/login")
+                    }
+                } else {
+                    console.log(error);
+                }
+            })
+        }
+        componentDidMount() {
         this.getUser()
     }
+    // function add -> untuk memberikan inisialisasi data dan menampilkan modal untuk menambah data
+    Add = () => {
+        $("#modal_user").modal("show")        
+        this.setState({
+            action: "insert",
+            id_user: 0,
+            nama_user: "",
+            username: "",
+            password: "",
+            role: "",
+            fillPassword: true
+        })
+    }
 
-// function add -> untuk memberikan inisialisasi data dan menampilkan modal untuk menambah data
-Add = () => {
-    $("#modal_member").modal("show")        
-    this.setState({
-        action: "insert",
-        id_user: 0,
-        nama_user: "",
-        username: "",
-        password: "",
-        role: "",
-        fillPassword: true
-    })
-}
+    // function saveUser -> untuk menyimpan data pada db dengan mngakses API
+        saveUser = event => {
+            event.preventDefault()
+            $("#modal_user").modal("hide")
+            let form = {
+                id_user: this.state.id_user,
+                nama_user: this.state.nama_user,
+                role: this.state.role,
+                username: this.state.username
+            }
+            
+            if (this.state.fillPassword) {
+                form.password =  this.state.password
+            }
+    
+            let url = base_url + "/user"
+            if (this.state.action === "insert") {
+                axios.post(url, form, this.headerConfig())
+                .then(response => {
+                    window.alert(response.data.message)
+                    this.getUser()
+                })
+                .catch(error => console.log(error))
+            } else if(this.state.action === "update") {
+                axios.put(url, form, this.headerConfig())
+                .then(response => {
+                    window.alert(response.data.message)
+                    this.getUser()
+                })
+                .catch(error => console.log(error))
+            }
+        }
+ 
+    // function edit -> untuk memberikan inisialisasi data dan menampilkan modal untuk mengedit data
+        Edit = selectedItem => {
+            $("#modal_user").modal("show")
+            this.setState({
+                action: "update",
+                id_user: selectedItem.id_user,
+                nama_user: selectedItem.nama_user,
+                username: selectedItem.username,
+                password: "",
+                role: selectedItem.role,
+                fillPassword: false
+            })
+        }
 
-// function edit -> untuk memberikan inisialisasi data dan menampilkan modal untuk mengedit data
-Edit = selectedItem => {
-    $("#modal_member").modal("show")
-    this.setState({
-        action: "update",
-        id_user: selectedItem.id_user,
-        nama_user: selectedItem.nama_user,
-        username: selectedItem.username,
-        password: "",
-        role: selectedItem.role,
-        fillPassword: false
-    })
-}
-
+    
+     // dropUser -> untuk menghapus data user
+        dropUser = selectedItem => {
+            if (window.confirm("Yakin mau dihapus ?")) {
+                let url = base_url + "/user/" + selectedItem.id_user
+                axios.delete(url, this.headerConfig())
+                .then(response => {
+                    window.alert(response.data.message)
+                    this.getUser()
+                })
+                .catch(error => console.log(error))
+            }
+        }
+    
 
     render(){
         return(
-            <div> 
-                <Navbar />
+            <div>
+                <Navbar role={this.state.role}/>
                 <div className="container">
                     <h3 className="text-bold text-info mt-2">
                         User List
@@ -129,8 +175,8 @@ Edit = selectedItem => {
                                         onClick={() => this.Edit(item)}>
                                             Edit
                                         </button>
-
-                                        <button className="btn btn-sm btn-info m-1"
+ 
+                                        <button className="btn btn-sm btn-danger m-1"
                                         onClick={() => this.dropUser(item)}>
                                             Delete
                                         </button>
@@ -142,7 +188,7 @@ Edit = selectedItem => {
                     <button className="btn btn-success" onClick={() => this.Add()}>
                         Add User
                     </button>
-
+ 
                     {/* Modal User */}
                     <div className="modal fade" id="modal_user">
                         <div className="modal-dialog">
@@ -158,12 +204,12 @@ Edit = selectedItem => {
                                         onChange={ev => this.setState({nama_user: ev.target.value})} 
                                         required
                                         />
-
+ 
                                         Role
                                         <div className="form-group">
-                                            <select name="role" id="role exampleFormControlSelect1" className="form-control"
+                                            <select name="role" id="role" className="form-control"
                                             onChange={ev => this.setState({role: ev.target.value})}
-                                             value={this.state.role}>
+                                            id_user="exampleFormControlSelect1" value={this.state.role}>
                                                 <option>--- Pilih ---</option>     
                                                 <option value="Admin">
                                                     Admin
@@ -173,14 +219,14 @@ Edit = selectedItem => {
                                                 </option>
                                             </select>
                                         </div>
-
+ 
                                         Username
                                         <input type="text" className="form-control mb-1"
                                         value={this.state.username}
                                         onChange={ev => this.setState({username: ev.target.value})}
                                         required
                                         />
-
+ 
                                         { this.state.action === "update" && this.state.fillPassword === false ? (
                                             <button className="btn btn-sm btn-secondary mb-1 btn-block"
                                             onClick={() => this.setState({fillPassword: true})}>
@@ -196,7 +242,7 @@ Edit = selectedItem => {
                                                 />
                                             </div>
                                         ) }
-
+ 
                                         <button type="submit" className="btn btn-block btn-success">
                                             Simpan
                                         </button>
@@ -209,6 +255,4 @@ Edit = selectedItem => {
             </div>
         )
     }
-
 }
-        
